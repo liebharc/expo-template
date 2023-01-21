36da45a2ecd1ba8300dc655f97c18f701d033421
Christian Liebhardt <christian.liebhardt@arcor.de>
2023-01-21 08:48:00 +0100

    npx expo install expo-random

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, NativeModules } from 'react-native';
+
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>	
+	  <Text>{NativeModules.ExpoRandom !== null ? 'Random' : 'No random'}</Text>
+			<Text>
+				{NativeModules.RandomModule !== null ? 'Random' : 'No random'}
+			</Text>
+			<Text>{Object.keys(NativeModules).join(', ')}</Text>
+			<Text>{Constants.systemFonts}</Text>
       <StatusBar style="auto" />
     </View>
   );
